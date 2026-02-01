988e70cf5615ef49a4f37749a72abe7c80ce938d
Austin Lord <austin.lord@gmail.com>
2026-02-01 12:05:23 -0500

    log

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Plog",
   description: "Personal Log",
+  icons: {
+    icon: '/log.svg',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
